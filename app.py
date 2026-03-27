72c60f05b7571c5d022939b25cb706d021f37a3f
4yu5hi <ayushi.24mip10174@vitbhopal.ac.in>
2026-03-28 01:56:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
 import pickle
 
 st.title("Big Mart Sales Prediction")
 
-# Example inputs (you can expand later)
+# inputs
 item_weight = st.number_input("Item Weight", value=10.0)
 item_visibility = st.number_input("Item Visibility", value=0.05)
 item_mrp = st.number_input("Item MRP", value=100.0)
 outlet_year = st.number_input("Outlet Establishment Year", value=2000)
 
+# load model
+model = pickle.load(open("model.pkl", "rb"))
+
 if st.button("Predict"):
-    st.success("Prediction logic will go here")
+    input_data = np.zeros(model.n_features_in_)
+    
+    # manually set important features
+    input_data[0] = item_weight
+    input_data[1] = item_visibility
+    input_data[2] = item_mrp
+    input_data[3] = outlet_year
+
+    input_data = input_data.reshape(1, -1)
+
+    prediction = model.predict(input_data)
+
+    st.success(f"Predicted Sales: {round(prediction[0],2)}")
